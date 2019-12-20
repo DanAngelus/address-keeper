1bd2b9bbe7b7ea3a02914b18fea939b3bf697357
dbennett_tier2 <dbennett@tier2consulting.com>
2019-12-20 11:49:07 +0000

    Initial commit

---
+++ src/main/java/uk/danangelus/address/AddressKeeperApplication.java
@@ -0,0 +1,13 @@
+package uk.danangelus.address;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AddressKeeperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AddressKeeperApplication.class, args);
+	}
+
+}
